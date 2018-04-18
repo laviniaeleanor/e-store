acdf642dc0ea1d0493aae28ae99bd888594fc810
laviniaeleanor <laviniaeleanor@outlook.com>
2018-04-18 12:03:18 +0200

    Basic structure client side, with redux, store set up and basic folder tree(empty files)

---
+++ client/src/App.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Redirect } from 'react-router-dom'
+import { ProductsList } from './components/ProductsList'
+import { ProductDetails } from './components/ProductDetails'
+import './App.css';
+
+class App extends Component {
+    render() {
+        return (
+            <Router>
+                <div>
+                    <Route exact path="/products" component={ProductsList} />
+                    <Route exact path="/products/:id" component={ProductDetails} />
+                    <Route exact path="/" render={ () => <Redirect to="/products" /> } />
+                </div>
+            </Router>
+        )
+    }
+}
+
+export default App;
